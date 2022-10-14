3c017b419c497124ed5317e2e4c9e0dee3db1085
Liam Ouyang <56122766+liam-oyh@users.noreply.github.com>
2022-10-14 03:28:45 +0000

    looping

--- index.js
+++ index.js
@@ -2,11 +2,34 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser'); 
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+// Connect to database
+let myURI = process.env['MONGO-URI'];
+var urlShort = mongoose.connect(myURI, { UseNewUrlParser: true, UseUnifiedTopology: true});
+
+// Create a schema
+let urlSchema = new mongoose.Schema({
+  inputUrl: {
+    type: String,
+    required: true
+  },
+  shortUrl: {
+    type: Number,
+    default: 0
+  } , {collection: "urls"});
+
+// Create a model
+const Url = mongoose.model('Url', urlSchema);
+
+// basic settings
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,9 +38,21 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+// urlId = collection length 
+app.route('/api/shorturl').get(function(req, res, next) {
+    res.json({"original_url": req.body.url, "short_url": urlId});
+    next();
+  })
+  .post(function(req,res){
+    var item = {
+      inputUrl: req.body.url,
+      shortUrl: urlId
+   };
+    var data = new Url(item);
+    data.save();
+
+       res.redirect('/');
+  })
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
